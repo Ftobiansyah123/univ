551b2adb6c257e61d9c536147912c6799dc4cf2c
Ftobiansyah123 <ftobiansyah@gmail.com>
2021-07-20 21:42:00 -0700

    benerin link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fadli Tobiansyah
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://api.whatsapp.com/send?phone=6288245623765&text=Hallo%20Anak%20Baik">Whatsapp</a>
+                    <a href="https://www.instagram.com/penemu_ganjil/">Intagram</a>
+                    <a href="https://github.com/Ftobiansyah123">GitHub</a>
                 </div>
             </div>
         </div>
